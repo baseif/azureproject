5798fbbaf206ad214948337bd53ce6427ff1d805
Ben Amara Seif Allah <baseif@ymail.com>
2017-06-21 14:08:29 +0200

    Update Script-env.ps1

--- Script-env.ps1
+++ Script-env.ps1
@@ -9,7 +9,6 @@ Set-ExecutionPolicy unrestricted
 net use "s:" "\\$Storageaccount.file.core.windows.net\$path" /u:"$Storageaccount" $storageAccesskeys
 
 copy-item S:\SourcesInstallVM\ c:\ -recurse
-Copy-Item S:\SourcesInstallVM\SourceDSCprerequisites C:\SourcesInstallVM\SourceDSCprerequisites -recurse
 cd C:\SourcesInstallVM\SourceDSCprerequisites
 wusa.exe C:\SourcesInstallVM\SourceDSCprerequisites\$($(get-childitem "*.msu").Name) /quiet /norestart
 while (Get-Process "wusa" -ErrorAction silentlycontinue) {"WMF 4.0 is being instaled, please wait..."; start-sleep -seconds 5}
@@ -18,3 +17,11 @@ cd (get-childitem -Recurse | where { $_.Name -match "Install-WMF*" }).DirectoryN
 while (Get-Process "wusa" -ErrorAction silentlycontinue) {"WMF 5.1 is being instaled, please wait..."; start-sleep -seconds 5}
 Enable-PSRemoting -Force
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
+
+
+
+# Name of your network adapter in Windows
+$netAdapterName = (Get-NetAdapter).Name
+# Disable IPv6
+Disable-NetAdapterBinding -InterfaceAlias $netAdapterName -ComponentID ms_tcpip6
+restart-computer -force
