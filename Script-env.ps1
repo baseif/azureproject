32f7fb1f965aff001e49ba4a5896ffe7391633c5
Ben Amara Seif Allah <baseif@ymail.com>
2017-06-26 14:08:29 +0200

    Update Script-env.ps1

--- Script-env.ps1
+++ Script-env.ps1
@@ -1,10 +1,18 @@
-
+param (
+    [string]$storageAccesskeys,
+      [string]$Storageaccount,
+      [string]$path
+    )
 Set-ExecutionPolicy unrestricted
+
+
+diskpart /s C:\SourcesInstallVM\SourceDSCprerequisites\DiskPartScript.txt > C:\SourcesInstallVM\SourceDSCprerequisites\Log_DiskPart.log
 
 New-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\' -Name  'DisabledComponents' -Value '0xff' -PropertyType 'DWord'
 
+net use "s:" "\\$Storageaccount.file.core.windows.net\$path" /u:"$Storageaccount" $storageAccesskeys
 
-copy-item h:\SourcesInstallVM\ c:\ -recurse
+copy-item S:\SourcesInstallVM\ c:\ -recurse
 cd C:\SourcesInstallVM\SourceDSCprerequisites
 wusa.exe C:\SourcesInstallVM\SourceDSCprerequisites\$($(get-childitem "*.msu").Name) /quiet /norestart
 while (Get-Process "wusa" -ErrorAction silentlycontinue) {"WMF 4.0 is being instaled, please wait..."; start-sleep -seconds 5}
@@ -12,5 +20,3 @@ cd (get-childitem -Recurse | where { $_.Name -match "Install-WMF*" }).DirectoryN
 & (get-childitem -Recurse | where { $_.Name -match "Install-WMF*" }).FullName
 while (Get-Process "wusa" -ErrorAction silentlycontinue) {"WMF 5.1 is being instaled, please wait..."; start-sleep -seconds 5}
 Enable-PSRemoting -Force
-
-restart-computer -force		
