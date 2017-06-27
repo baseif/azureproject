bb5e1d5244b5540a8a4ae856e1c741b53b69a1cf
Ben Amara Seif Allah <baseif@ymail.com>
2017-06-27 14:39:29 +0200

    Update Script-env.ps1

--- Script-env.ps1
+++ Script-env.ps1
@@ -5,17 +5,17 @@ param (
     )
 Set-ExecutionPolicy unrestricted
 
-
-New-item -ItemType file C:\SourcesInstallVM\SourceDSCprerequisites\Log_DiskPart.log
-diskpart /s C:\SourcesInstallVM\SourceDSCprerequisites\DiskPartScript.txt >> C:\SourcesInstallVM\SourceDSCprerequisites\Log_DiskPart.log
-
-
 New-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\' -Name  'DisabledComponents' -Value '0xff' -PropertyType 'DWord'
 
 net use "s:" "\\$Storageaccount.file.core.windows.net\$path" /u:"$Storageaccount" $storageAccesskeys
 
 copy-item S:\SourcesInstallVM\ c:\ -recurse
+
 cd C:\SourcesInstallVM\SourceDSCprerequisites
+
+New-item -ItemType file C:\SourcesInstallVM\SourceDSCprerequisites\Log_DiskPart.log
+diskpart /s C:\SourcesInstallVM\SourceDSCprerequisites\DiskPartScript.txt >> C:\SourcesInstallVM\SourceDSCprerequisites\Log_DiskPart.log
+
 wusa.exe C:\SourcesInstallVM\SourceDSCprerequisites\$($(get-childitem "*.msu").Name) /quiet /norestart
 while (Get-Process "wusa" -ErrorAction silentlycontinue) {"WMF 4.0 is being instaled, please wait..."; start-sleep -seconds 5}
 cd (get-childitem -Recurse | where { $_.Name -match "Install-WMF*" }).DirectoryName
