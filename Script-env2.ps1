e3917785cfdcdf5e682ed6bc4779694c59f036fb
Ben Amara Seif Allah <baseif@ymail.com>
2017-07-04 17:06:02 +0200

    Update Script-env2.ps1

--- Script-env2.ps1
+++ Script-env2.ps1
@@ -1,5 +1,5 @@
 
-netsh advfirewall firewall del rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in
+netsh advfirewall firewall del rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in ;
 
 
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=block
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=block ;
